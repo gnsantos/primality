c9a1bd8b1e3910464135c570a924681163bba9d5
Gervasio Santos <gervasio.neto94@gmail.com>
2013-08-31 13:43:02 -0300

    Interface necessary to acess the functions needed in the Miller-Rabin test

---
+++ Miller-Rabin/rabinFunctions.h
@@ -0,0 +1,13 @@
+#ifndef H_FUNCTIONS_DEFINE
+#define H_FUNCTIONS_DEFINE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+typedef enum{FALSE, TRUE}Boolean;
+
+Boolean millerRabinTest(unsigned long, int);
+
+#endif
