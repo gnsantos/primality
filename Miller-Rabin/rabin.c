cdd00d2256d2dd15782b8954253beeac91ebfadd
Gervasio Santos <gervasio.neto94@gmail.com>
2013-09-18 19:54:11 -0300

    Mais testes realizdos com o MRT

--- Miller-Rabin/rabin.c
+++ Miller-Rabin/rabin.c
@@ -7,14 +7,14 @@
 void help();
 
 int main(int argc, char* argv[]){
-  char c, *outputName, *inputName;
-  FILE* input;
-  FILE* output;
+  char c;
+  FILE* input = NULL;
+  FILE* output = NULL;
   Boolean runtime = TRUE, single = FALSE, result;
   unsigned long p = 2;
-  int runs = 30, primes = 0, composites = 0;
+  int runs = 30, primes = 0, composites = 0, i =0;
   clock_t start, end;
-  double elapsed = 0;
+  double elapsed = 0, mid = 0, parcial = 0;
 
   if(argc < 2){
     help();
@@ -23,14 +23,14 @@ int main(int argc, char* argv[]){
   while( (c = getopt(argc, argv, "t:f:o:r:y")) != -1){
     switch(c){
     case 'f':
-      inputName = optarg;
+      input = fopen(optarg, "r");
       break;
     case 't':
       p = atoi(optarg);
       single = TRUE;
       break;
     case 'o':
-      outputName = optarg;
+      output = fopen(optarg, "w");
       break;
     case 'y':
       runtime = FALSE;
@@ -38,40 +38,45 @@ int main(int argc, char* argv[]){
     case 'r':
       runs = atoi(optarg);
       break;
+    default:
+      help();
+      break;
     }
   }
-
-  output = fopen(outputName, "w");
-  input = fopen(inputName, "r");
-
+  
   if(!single){
     if(input == NULL){
       puts("Unable to open file for reading.");
       help();
     }
-    while(fscanf(input, "%lud", &p) != EOF){\
+    while(fscanf(input, "%lud", &p) != EOF){
+      parcial = 0;
+      for(i = 0; i < 120; i++){
+	start = clock();
 
-      start = clock();
+	result = millerRabinTest(p, runs);
+	
+	end = clock();
 
-      result = millerRabinTest(p, runs);
-
-      end = clock();
-
-      elapsed += (double)(end - start)/CLOCKS_PER_SEC;
-
+	mid = (double)(end - start)/CLOCKS_PER_SEC;
+     
+	parcial += mid;
+      }
+      
+      elapsed += parcial;
       if(result){
-	if(!output) fprintf(stdout, "%ld is a prime.\n", p);
-	else fprintf(output, "%ld is a prime.\n", p);
+	if(!output) fprintf(stdout, "%ld %f\n", p, parcial);
+	else fprintf(output, "%ld %f.\n", p, parcial);
 	primes++;
       }
       else{
-	if(!output) fprintf(stdout, "%ld is composite.\n", p);
-	else fprintf(output, "%ld is composite.\n", p);
+	if(!output) fprintf(stdout, "%ld %f.\n", p, parcial);
+	else fprintf(output, "%ld %f.\n", p, parcial);
 	composites++;
       }
     }
     
-    fprintf(stdout, "There were %d composites and %d primes.\n", composites, primes);
+    /*fprintf(stdout, "There were %d composites and %d primes.\n", composites, primes);*/
     if(runtime) fprintf(stdout, "TIME: %f (reading and printing times not included)\n", elapsed);
 
   }
@@ -85,31 +90,31 @@ int main(int argc, char* argv[]){
     elapsed = (double)(end - start)/CLOCKS_PER_SEC;
 
     if(result){
-      if(!output) fprintf(stdout, "%ld is a prime.\n", p);
-      else fprintf(output, "%ld is a prime.\n", p);
+      if(!output) fprintf(stdout, "%ld %f.\n", p, elapsed);
+      else fprintf(output, "%ld %f.\n", p, elapsed);
     }
     else{
-      if(!output) fprintf(stdout, "%ld is composite.\n", p);
-      else fprintf(output, "%ld is composite.\n", p);
+      if(!output) fprintf(stdout, "%ld is %f.\n", p, elapsed);
+      else fprintf(output, "%ld is %f.\n", p, elapsed);
     }
 
     if(runtime) fprintf(stdout, "TIME: %f (reading and printing times not included)\n", elapsed);
 
   }
 
-  fclose(input);
-  fclose(output);
+  if(input) fclose(input);
+  if(output) fclose(output);
 
   return 0;
 }
 
 void help(){
   fprintf(stdout, "Use -t<number> to test the primality of a single number.\n"
-	 "Use -f<input file name> to read the numbers from a file. The output will the STDOUT.\n"
-	 "To redirect outuput, use -o<output file name>.\n"
-	 "The time consumed is sent to STDOUT. To supress, use -y.\n"
-	 "The standard number of runs is 30. Change it using -r<nRuns>.\n"
-	 );
+	  "Use -f<input file name> to read the numbers from a file. The output will the STDOUT.\n"
+	  "To redirect outuput, use -o<output file name>.\n"
+	  "The time consumed is sent to STDOUT. To supress, use -y.\n"
+	  "The standard number of runs is 30. Change it using -r<nRuns>.\n"
+	  );
   exit(-1);
 }
     
